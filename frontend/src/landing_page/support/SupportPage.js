3ae9e8cd4fa0ed9c54729ebe8075576aabbfab04
Rohit Jat <rohitlamba97315@gmail.com>
2026-01-23 19:56:51 +0530

    Fix: Remove nested git repositories and add full project files

---
+++ frontend/src/landing_page/support/SupportPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hero from './Hero';
+import CreateTicket from './CreateTicket'
+
+function PricingPage() {
+    return ( 
+        <>
+            <Hero/>
+            <CreateTicket/>
+        </>
+     );
+}
+
+export default PricingPage;
